c2dc16c6fa6c8bcec71abab9f65aea93d15c67b8
GLORY-JAIN <glory.jain@kalvium.community>
2025-09-05 12:33:24 +0530

    initial project setup with readme and welcome program

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,9 +2,10 @@
 package com.school;
 
 public class Main {
-    public Main() { }
+   public Main() {
+   }
 
    public static void main(String[] var0) {
-      System.out.println("Welcome to the Attendance Management System!");
+     System.out.println("Welcome to the Attendance Management System!");
    }
 }
